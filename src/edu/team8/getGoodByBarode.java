7e3adb58c07dc546b18f4346545c402f50425202
韩知昊 <dalenew@163.com>
2016-01-06 19:06:42 +0800

    整合 韩知昊

---
+++ src/edu/team8/getGoodByBarode.java
@@ -0,0 +1,11 @@
+package edu.team8;
+import java.sql.*;
+/**
+ * Created by 知昊 on 2016/1/6.
+ */
+public class getGoodByBarode {
+
+    public getGoodByBarode() {
+        Connection sqlConnect;
+    }
+}
