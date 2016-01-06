476237f0a88e4030e335077bb332e66d9e4b956e
韩知昊 <dalenew@163.com>
2016-01-06 13:44:56 +0800

    建类
    基础类建立

---
+++ src/edu/team8/classes/Good.java
@@ -0,0 +1,52 @@
+package edu.team8.classes;
+
+
+/**
+ * Created by 知昊 on 2016/1/6.
+ */
+public class Good {
+    protected String barcode;
+    protected String name;
+    protected String unit;
+    protected float price;
+
+    public Good(String barcode, String name, String unit, float price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public String getBarcode()
+    {
+        return this.barcode;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getUnit()
+    {
+        return this.unit;
+    }
+    public float getPrice()
+    {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public void setBarcode(String barcode) {
+
+        this.barcode = barcode;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+
+}
