b1a7e0142691c49bac22fac5f651bf80220910ea
13080107 <491094601@qq.com>
2016-01-06 19:34:14 +0800

    差不多

---
+++ test/edu/team8/PrintListTest.java
@@ -0,0 +1,16 @@
+package edu.team8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wl on 2016/1/6.
+ */
+public class PrintListTest {
+
+    @Test
+    public void testPrintGoodList() throws Exception {
+
+    }
+}
