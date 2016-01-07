8a53c0d3fb28900c12583733148f3ce76b99c979
13080107 <491094601@qq.com>
2016-01-07 13:15:16 +0800

    保留小数

--- src/edu/team8/PrintList.java
+++ src/edu/team8/PrintList.java
@@ -1,6 +1,7 @@
 package edu.team8;
 import  edu.team8.classes.*;
 
+import java.text.DecimalFormat;
 import java.util.*;
 
 /**
@@ -9,17 +10,18 @@ import java.util.*;
 public interface PrintList {
     public static void PrintGoodList(ArrayList<GoodExtends> xiaoyu,BarcodeScanner bs){
         double sum=0;                          //总价
+        DecimalFormat df = new DecimalFormat("#.00");                              //保留两位小数格式
         bs.printReceipt("\t                     商店购物小票清单");                            //表头信息
-        bs.printReceipt("==============================================================");
-        bs.printReceipt("商品条码\t名称\t数量\t单价\t小计\t");
+        bs.printReceipt("==========================================================");
+        bs.printReceipt("商品条码\t名称\t数量\t单价（元）\t小计（元）\t");
         for(GoodExtends ge:xiaoyu)
         {
             bs.printReceipt(ge.getBarcode() + "\t" + ge.getName() + "\t" + ge.getCount() + ge.getUnit() + "\t"
-                    + ge.getPrice() + "\t" + ge.getCount()*ge.getPrice());                //打印信息
+                    + df.format(ge.getPrice()) + "\t" + df.format(ge.getCount()*ge.getPrice()));                //打印信息
                   sum+=  ge.getCount()*ge.getPrice();
         }
         bs.printReceipt("------------------------------");
-        bs.printReceipt("总计:" + sum);
-        bs.printReceipt("*****************************************************************************");
+        bs.printReceipt("总计:" + df.format(sum));
+        bs.printReceipt("*********************************************************************************");
     }
 }
