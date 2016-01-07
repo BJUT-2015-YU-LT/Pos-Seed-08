26a9427fe377949c5bc2ce4c84533546e1c3fa16
13080107 <491094601@qq.com>
2016-01-07 12:20:51 +0800

    更改打印

--- src/edu/team8/PrintList.java
+++ src/edu/team8/PrintList.java
@@ -9,11 +9,13 @@ import java.util.*;
 public interface PrintList {
     public static void PrintGoodList(ArrayList<GoodExtends> xiaoyu){
         double sum=0;                          //总价
-        System.out.println("***商店购物小票清单***");
+        System.out.println("\t\t\t\t商店购物小票清单");                                        //表头信息
+        System.out.println("=========================================================");
+        System.out.println("商品条码\t\t名称\t\t数量\t\t单价\t\t小计\t\t");
         for(GoodExtends ge:xiaoyu)
         {
-            System.out.println("商品BarCode:   " + ge.getBarcode() + "   " + "名称:   " + ge.getName() + "   " +  //打印信息
-                    "数量:   " + ge.getCount() + ge.getUnit() + ",   " + "单价:   " + ge.getPrice() + "   " + "小计:   " + ge.getCount()*ge.getPrice());
+            System.out.println(ge.getBarcode() + "\t\t" + ge.getName() + "\t\t" + ge.getCount() + ge.getUnit() + "\t\t\t"
+                    + ge.getPrice() + "\t\t\t" + ge.getCount()*ge.getPrice());                //打印信息
                   sum+=  ge.getCount()*ge.getPrice();
         }
         System.out.println("---------------");
