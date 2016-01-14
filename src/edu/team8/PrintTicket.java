c27425949900410cbf167605f70c72f4e24e01ce
13080107 <491094601@qq.com>
2016-01-14 13:30:52 +0800

    打印格式校正

--- src/edu/team8/PrintTicket.java
+++ src/edu/team8/PrintTicket.java
@@ -19,17 +19,17 @@ public interface PrintTicket {
         DecimalFormat df = new DecimalFormat("0.00");                              //保留两位小数格式
         DateFormat dft = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss ", Locale.CHINA);       // 时间格式
 
-        bs.printReceipt("\t                     商店购物小票清单");                            //表头信息
-        bs.printReceipt("\t\t       打印时间：  " + dft.format(d));                              //打印时间
-        bs.printReceipt("==========================================================");
-        bs.printReceipt("商品条码\t名称\t数量\t单价（元）\t小计（元）");
+        bs.printReceipt("\t                商店购物小票清单");                            //表头信息
+        bs.printReceipt("\t\t         打印时间：" + dft.format(d));                              //打印时间
+        bs.printReceipt("=======================================================");
+        bs.printReceipt("商品条码\t      名称\t 数量\t    单价(元)\t 小计(元)");
         for(GoodExtends ge:ti.getPaidList()) {
-                bs.printReceipt(ge.getBarcode() + "\t" + ge.getName() + "\t" + ge.getTotalCount() + ge.getUnit() + "\t"
-                        + df.format(ge.getPrice()) + "\t" + df.format(ge.getPaidCount() * ge.getPrice()));  //打印信息
+                bs.printReceipt(ge.getBarcode() + "      " + ge.getName() + "\t " + ge.getTotalCount() + ge.getUnit() + "\t    "
+                        + df.format(ge.getPrice()) + "\t " + df.format(ge.getPaidCount() * ge.getPrice()));  //打印信息
                 n += ge.getTotalCount();                                                      //计算商品总数
         }
         if(ti.getSavedList() !=null){                                                           //有赠送商品时才打印相关赠送信息
-            bs.printReceipt("-------------------------------------------");                                 //打印赠送商品
+            bs.printReceipt("-------------------------------------------------------");                 //打印赠送商品
             bs.printReceipt("赠送商品：");
             for(GoodExtends ge:ti.getSavedList()) {
                 bs.printReceipt("名称：" + ge.getName() + "    数量：" + (ge.getTotalCount()-ge.getPaidCount()));  //打印信息
@@ -37,9 +37,9 @@ public interface PrintTicket {
             }
         }
 
-        bs.printReceipt("-------------------------------------------");                                 //打印表尾
-        bs.printReceipt("总计：" + df.format(ti.getPaidPrice()) + "（元）" + "  节省：" + df.format(ti.getSavedPrice()) + "（元）" +
-                "  商品总数：" + n +"（件）");
-        bs.printReceipt("**********************************************************************************");
+        bs.printReceipt("-------------------------------------------------------");                    //打印表尾
+        bs.printReceipt("总计：" + df.format(ti.getPaidPrice()) + "(元)" + "  节省：" + df.format(ti.getSavedPrice()) + "(元)" +
+                "  商品总数：" + n +"(件)");
+        bs.printReceipt("*******************************************************");
     }
 }
--- test/edu/team8/PrintTicketTest.java
+++ test/edu/team8/PrintTicketTest.java
@@ -2,6 +2,8 @@ package edu.team8;
 
 import edu.team8.classes.Good;
 import edu.team8.classes.GoodExtends;
+import edu.team8.classes.TicketInfo;
+import edu.team8.classes.Vip;
 import org.junit.Test;
 
 
@@ -16,26 +18,50 @@ public class PrintTicketTest {
 
     @Test
     public void testPrintTicketList() throws Exception {
-        int sum = 0;
-        ArrayList<GoodExtends> t=new ArrayList<GoodExtends>();
+        TicketInfo info1 = new TicketInfo();
+        TicketInfo info2 = new TicketInfo();
+
+        ArrayList<GoodExtends> p1=new ArrayList<GoodExtends>();
+        ArrayList<GoodExtends> p2=new ArrayList<GoodExtends>();
+        ArrayList<GoodExtends> s1=new ArrayList<GoodExtends>();
+        ArrayList<GoodExtends> s2=new ArrayList<GoodExtends>();
+
+        Vip v1= new Vip("vip01",50);
+        Vip v2= new Vip("vip02",-10);
+
+
         GoodExtends ge_1 =new GoodExtends("ITEM000001","红牛","罐",6.5,1,1, Good.NORMAL,4,4);  //正确
-        t.add(ge_1);
+        p1.add(ge_1);
 
         GoodExtends ge_2 =new GoodExtends("ITEM000002","黑牛","头",100,0.8,0.95 , Good.NORMAL,2,2);  //正确
-        t.add(ge_2);
+        p2.add(ge_2);
 
         GoodExtends ge_3 =new GoodExtends("ITEM000002","黑牛","头",100,2,0.95, Good.PROMOTION,3,2);  //重复
-        t.add(ge_3);
+        s1.add(ge_3);
 
         GoodExtends ge_4 =new GoodExtends("本应是条码","321","233",-10,-15,-5, Good.DISCOUNT,2,3);  //错误
-        t.add(ge_4);
-        for(GoodExtends ge:t)
+        s2.add(ge_4);
+
+        info1.setPaidList(p1);
+        info1.setSavedList(s1);
+        info1.setPaidPrice(6);
+        info1.setSavedPrice(2);
+        info1.setVipInfo(v1);
+
+
+        info2.setPaidList(p2);
+        info2.setSavedList(s2);
+
+        /*for(GoodExtends ge:t)
         {
             System.out.println("商品BarCode:   " + ge.getBarcode() + "   " + "名称:   " + ge.getName() + "   " +  //打印信息
                     "数量:   " + ge.getTotalCount() + ge.getUnit() + ",   " + "单价:   " + ge.getPrice() + "   "
                     + "小计:   " + ge.getTotalCount()*ge.getPrice());
             sum+=  ge.getTotalCount()*ge.getPrice();
         }
-        System.out.println("总计:" + sum);
+        System.out.println("总计:" + sum);*/
+        PosGUI pg = new PosGUI("test");
+        PrintTicket.PrintTicketList(info1,pg);
+
     }
 }
